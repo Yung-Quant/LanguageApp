a861065d8ffdf150fef3b4a2db139d14249e24b0
Shane Kennedy <shaneikennedy@Shanes-MacBook-Pro.local>
2017-07-12 00:28:26 -0400

    remove lines from previous debugging

--- routes/findNew.js
+++ routes/findNew.js
@@ -6,7 +6,7 @@ var auth = require('../config/auth.js');
 router.get('/', auth.facebook.ensureAuthenticated, function(req, res){
     user = req.user._doc;
     //check for users who know the langs that the requester s interested in
-    Users.find({knownLangs: {"$in": user.knownLangs}}, function(err, docs){
+    Users.find({interestLangs: {"$in": user.knownLangs}}, function(err, docs){
         if(err) throw err;
         else{
             res.render('findNew', {results: docs, user: user});
